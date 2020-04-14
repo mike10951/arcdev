22850e2c044dfdc7ac824ecf10f0f0c6dcd65714
Miguel Cabrera <miguel.cabrera.1095@gmail.com>
2020-04-14 15:53:31 -0500

    Initial setup, toolbar and menu for an enterprise website
    Using material-ui, react, and react-router

---
+++ src/components/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import theme from './ui/Theme';
+import Header from '../components/ui/Header';
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={() => <div>Home</div>} />
+          <Route exact path='/services' component={() => <div>services</div>} />
+          <Route
+            exact
+            path='/customsoftware'
+            component={() => <div>customsoftware</div>}
+          />
+          <Route
+            exact
+            path='/mobileapps'
+            component={() => <div>mobileapps</div>}
+          />
+          <Route exact path='/websites' component={() => <div>websites</div>} />
+          <Route
+            exact
+            path='/revolution'
+            component={() => <div>revolution</div>}
+          />
+          <Route exact path='/about' component={() => <div>about</div>} />
+          <Route exact path='/contact' component={() => <div>contact</div>} />
+          <Route exact path='/estimate' component={() => <div>estimate</div>} />
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/components/ui/Header.js
@@ -0,0 +1,273 @@
+import React, { useState, useEffect } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import { makeStyles } from '@material-ui/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import { Link } from 'react-router-dom';
+
+import logo from '../../assets/logo.svg';
+
+function ElevationScroll(props) {
+  const { children } = props;
+
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+const useStyles = makeStyles((theme) => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar,
+    marginBottom: '3em',
+  },
+  logo: {
+    height: '8em',
+  },
+  tabContainer: {
+    marginLeft: 'auto',
+  },
+  tab: {
+    ...theme.typography.tab,
+    minWidth: 10,
+    marginLeft: '25px',
+  },
+  button: {
+    ...theme.typography.estimate,
+    borderRadius: '50px',
+    marginLeft: '50px',
+    marginRight: '25px',
+    height: '45px',
+  },
+  logoContainer: {
+    padding: 0,
+    '&:hover': {
+      backgroundColor: 'transparent',
+    },
+  },
+  menu: {
+    backgroundColor: theme.palette.common.blue,
+    color: 'white',
+    borderRadius: '0px',
+  },
+  menuItem: {
+    ...theme.typography.tab,
+    opacity: 0.7,
+    '&:hover': {
+      opacity: 1,
+    },
+  },
+}));
+
+export default function Header(props) {
+  const classes = useStyles();
+  const [value, setValue] = useState(0);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [open, setOpen] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const handleChange = (e, value) => {
+    setValue(value);
+  };
+
+  const handleClick = (e) => {
+    setAnchorEl(e.currentTarget);
+    setOpen(true);
+  };
+
+  const handleMenuItemClick = (e, i) => {
+    setAnchorEl(null);
+    setOpen(false);
+    setSelectedIndex(i);
+  };
+
+  const handleClose = (e) => {
+    setAnchorEl(null);
+    setOpen(false);
+  };
+
+  const menuOptions = [
+    { name: 'Services', link: '/services' },
+    { name: 'Custom Software Development', link: '/customsoftware' },
+    { name: 'Mobile App Development', link: '/mobileapps' },
+    { name: 'Website Development', link: '/websites' },
+  ];
+
+  useEffect(() => {
+    if (window.location.pathname === '/' && value !== 0) {
+      setValue(0);
+      // TO-DO: When a service is clicked and the page is reloaded, the value resets to 0. Unexpected behaviour.
+    } else if (window.location.pathname === '/services' && value !== 1) {
+      setValue(1);
+    } else if (window.location.pathname === '/revolution' && value !== 2) {
+      setValue(2);
+    } else if (window.location.pathname === '/about' && value !== 3) {
+      setValue(3);
+    } else if (window.location.pathname === '/contact' && value !== 4) {
+      setValue(4);
+    } else if (window.location.pathname === '/estimate' && value !== 5) {
+      setValue(5);
+    }
+
+    switch (window.location.pathname) {
+      case '/':
+        if (value !== 0) {
+          setValue(0);
+        }
+        break;
+      case '/services':
+        if (value !== 1) {
+          setValue(1);
+          setSelectedIndex(0);
+        }
+        break;
+      case '/customsoftware':
+        if (value !== 1) {
+          setValue(1);
+          setSelectedIndex(1);
+        }
+        break;
+      case '/mobileapps':
+        if (value !== 1) {
+          setValue(1);
+          setSelectedIndex(2);
+        }
+        break;
+      case '/websites':
+        if (value !== 1) {
+          setValue(1);
+          setSelectedIndex(3);
+        }
+        break;
+      case '/revolution':
+        if (value !== 2) {
+          setValue(2);
+        }
+        break;
+      case '/about':
+        if (value !== 3) {
+          setValue(3);
+        }
+        break;
+      case '/contact':
+        if (value !== 4) {
+          setValue(4);
+        }
+        break;
+      case '/estimate':
+        if (value !== 5) {
+          setValue(5);
+        }
+        break;
+      default:
+        break;
+    }
+  }, [value]);
+
+  return (
+    <>
+      <ElevationScroll>
+        <AppBar position='fixed'>
+          <Toolbar disableGutters>
+            <Button
+              className={classes.logoContainer}
+              disableRipple
+              component={Link}
+              to='/'
+              onClick={() => setValue(0)}
+            >
+              <img src={logo} alt='company logo' className={classes.logo} />
+            </Button>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              className={classes.tabContainer}
+              indicatorColor='primary'
+            >
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to='/'
+                label='Home'
+              />
+              <Tab
+                aria-owns={anchorEl ? 'simple-menu' : undefined}
+                aria-haspopup={anchorEl ? 'true' : undefined}
+                className={classes.tab}
+                onMouseOver={(event) => handleClick(event)}
+                component={Link}
+                to='/services'
+                label='Services'
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to='/revolution'
+                label='The Revolution'
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to='/about'
+                label='About Us'
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to='/contact'
+                label='Contact Us'
+              />
+            </Tabs>
+            {/* TO-DO: When clicked, there's still an active tab in toolbar. Unexpected behaviour. */}
+            <Button
+              className={classes.button}
+              variant='contained'
+              color='secondary'
+              component={Link}
+              to='/estimate'
+              onClick={() => setValue(5)}
+            >
+              Free Estimate
+            </Button>
+            <Menu
+              id='simple-menu'
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              classes={{ paper: classes.menu }}
+              MenuListProps={{ onMouseLeave: handleClose }}
+              elevation={0}
+            >
+              {menuOptions.map((option, i) => (
+                <MenuItem
+                  key={option}
+                  component={Link}
+                  to={option.link}
+                  classes={{ root: classes.menuItem }}
+                  onClick={(event) => {
+                    handleMenuItemClick(event, i);
+                    setValue(1);
+                    handleClose();
+                  }}
+                  selected={i === selectedIndex && value === 1}
+                >
+                  {option.name}
+                </MenuItem>
+              ))}
+            </Menu>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <div className={classes.toolbarMargin} />
+    </>
+  );
+}
